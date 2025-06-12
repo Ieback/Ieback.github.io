58aa6bb40d146aa49109b1d242ee1317c41b0142
Ieback <zhulintao.chem@gmail.com>
2025-06-13 01:40:46 +0900

    chore: 更新footer

--- templates/footer.js
+++ templates/footer.js
@@ -1,7 +1,7 @@
 class Footer extends HTMLElement {
   connectedCallback() {
     const year = new Date().getFullYear();
-    this.innerHTML = `<footer class="footer">© <span id="year">${year}</span> Lebackの逃生舱</footer>`;
+    this.innerHTML = `<footer class="footer">© <span id="year">${year}</span> Leback的逃生舱</footer>`;
   }
 }
 customElements.define('blog-footer', Footer);
